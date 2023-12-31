dd34bc4713b52bb3c4874fc5706d7836b2b41e3b
HeisenbergCipherCracker <alimirmohammad.1386@gmail.com>
2023-12-31 15:00:23 -0500

    _

--- lib/intruder/intruder.py
+++ lib/intruder/intruder.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import threading
+import socket
 sys.path.append(os.getcwd())
 from lib.core.payloads.makesetsqlpayload import make_set_sql_payload
 from lib.core.payloads.substringpayload import sub_string_sql_inj
@@ -7,9 +9,10 @@ from utilis._regex.extractparam import replace_url_parameter
 from lib.core.request.POSt.post import SubData
 from lib.core.request.GET.get import Get
 from lib.logger.log import logger
+from  lib.core.parser.cmdline import time_out
 
 class Intruder(
-    object
+    threading.Thread
 ):
 
     def __init__(self,host,port):
@@ -19,22 +22,22 @@ class Intruder(
         self.result = []
         self.datasub = SubData(self.host,self.port)
         self.payload_url = None
+        self.timeout = socket.setdefaulttimeout(time_out if time_out is not None else 10)
 
 
-    def send_payload_substring(self):
+    def _send_payload_substring(self):
         if self.payload is None:
             rows = sub_string_sql_inj().split("\n") 
             sorted_rows = sorted(rows) 
             sorted_payload = "\n".join(sorted_rows)
             self.payload = sorted_payload
         
-        for payload in self.payload:
-            req = self.datasub.submit_data(self.host,payload)
+        for _ in self.payload:
+            req = self.datasub.submit_data(self.host,self.payload)
             self.result.append(self.datasub)
-            logger.info(self.result)
-            print(req)
+            # logger.info(self.result)
     
-    def send_substring_payload_requests_lib(self):
+    def _send_substring_payload_requests_lib(self):
         try:
             import requests
             if self.payload is None:
@@ -43,19 +46,30 @@ class Intruder(
                 sorted_payload = "\n".join(sorted_rows)
                 self.payload = sorted_payload
             
-            for payload in self.payload:
+            for _ in self.payload:
                 if self.payload_url is None:
-                    self.payload_url,_ = replace_url_parameter(self.host,payload)
+                    self.payload_url,_ = replace_url_parameter(url=self.host,new_value=self.payload)
                 req = requests.get(self.payload_url)
-                print(self.payload_url)
+                msg = "Testing:%s"%self.payload_url
+                logger.info(msg)
         
         except Exception as e:
-            print(e)
+            logger.error(str(e))
+    
+    def run_substring(self):
+        threads = [
+            self._send_payload_substring(),
+            self._send_substring_payload_requests_lib()
+        ]
+        for _ in threads:
+            _thread = threading.Thread(target=_)
+            _thread.start()
+            _thread.join()
                 
 
 
-# obj = Intruder("http://testfire.net/index.jsp?content=business_deposit.htm",8080)
-# obj.send_payload_substring()
+obj = Intruder("http://testfire.net/index.jsp?content=business_deposit.htm",8080)
+obj.run_substring()
         
         
 
--- sqlgo.py
+++ sqlgo.py
@@ -1,5 +1,9 @@
 from lib.core.parser.cmdline import attack
 from lib.core.tester.maintestcheck import main_exploit
+from lib.intruder.intruder import Intruder
 
 if __name__ == "__main__":
-    main_exploit()
+    # main_exploit()
+    obj = Intruder("http://testfire.net/index.jsp?content=business_deposit.htm",8080)
+    obj.run_substring()
+    
