3d138332d424b792e0006810173cff5967958888
HeisenbergCipherCracker <alimirmohammad.1386@gmail.com>
2024-01-02 14:20:19 -0500

    _

--- sqlgo.py
+++ sqlgo.py
@@ -1,11 +1,14 @@
 from lib.logger.log import logger
 from lib.core.tester.gather import gather_exploit
 from extra.disclaimer import disclaimer
+from extra.logo import logo
+
 
 
 
 def main():
     try:
+        print(logo)
         disclaimer()
         gather_exploit()
     except Exception as e:
