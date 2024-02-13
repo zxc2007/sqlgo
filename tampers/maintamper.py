13cabc5a95c8daed1be2582a251ad2d9d0d0dd68
HeisenbergCipherCracker <alimirmohammad.1386@gmail.com>
2024-02-13 18:17:18 -0500

    Greatest tamper

--- tampers/maintamper.py
+++ tampers/maintamper.py
@@ -39,6 +39,7 @@ import tampers.decenties
 import tampers.equaltolike
 import tampers.equaltorlike
 import tampers.escapequotes
+import tampers.greatests
 from src.core.parser.cmdline import tamper as _tamper
 
 def apply_tamper(payload):
@@ -78,6 +79,8 @@ def apply_tamper(payload):
         return tampers.equaltorlike.tamper(payload)
     elif _tamper == "escapequotes":
         return tampers.escapequotes.tamper(payload)
+    elif _tamper == "greatests":
+        return tampers.greatests.tamper(payload)
     elif _tamper is None:
         return payload
     else:
