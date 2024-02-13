6a45628103b384bef240e25b12f78845f839a4a1
HeisenbergCipherCracker <alimirmohammad.1386@gmail.com>
2024-02-12 21:55:00 -0500

    Copy right year

--- src/checker/check.py
+++ src/checker/check.py
@@ -1,7 +1,27 @@
+"""
+# SQLGO License - Version 1.1
+
+Copyright (C) 2023-2024 Heisenberg
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+"""
+
 import os
 import sys
 import re
-sys.path.append(os.getcwd())
 import src.core.setting.setting as settings
 
 def get_value_inside_boundaries(value):
--- src/core/request/infogather/findos.py
+++ src/core/request/infogather/findos.py
@@ -1,7 +1,29 @@
+"""
+# SQLGO License - Version 1.1
+
+Copyright (C) 2023-2024 Heisenberg
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+"""
 import os
 import sys
-sys.path.append(os.getcwd())
-import thirdparty.nmap as nmap
+try:
+    import thirdparty.nmap as nmap
+except:
+    pass
 from utilis._regex.getdomain import extract_domain
 from src.logger.log import logger
 
