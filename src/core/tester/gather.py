06f923c795156a7867c0ff122eb475aceb0f78b4
HeisenbergCipherCracker <alimirmohammad.1386@gmail.com>
2024-02-26 21:29:12 -0500

    Minor Fixes for #23

--- src/core/tester/gather.py
+++ src/core/tester/gather.py
@@ -200,7 +200,7 @@ def gather_exploit():
     
     except Exception as e:
         logger.debug(e)
-        if any(isinstance(e,ConnectionResetError),"Connection reset by peer" in str(e),"[Errno 54] Connection reset by peer" in str(e)):
+        if any([isinstance(e,ConnectionResetError),"Connection reset by peer" in str(e),"[Errno 54] Connection reset by peer" in str(e)]):
             logger.warning("It looks like that the target has been closed the connection due to the harmful requests.please try using the proxy or vpn.")
             logger.critical("This can be a protection of WAF/IPS against harmful requests.")
         
