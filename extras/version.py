b63b2f848ee1c8bcefe01b1ee6814457cdd27dba
HeisenbergCipherCracker <alimirmohammad.1386@gmail.com>
2024-03-02 16:18:35 -0500

    Draft new version

--- extras/version.py
+++ extras/version.py
@@ -18,6 +18,6 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
 """
-_VERSION = "1.3.5.8"
+_VERSION = "1.3.5.9"
 VERSION_TYPE = "#dev" if _VERSION.count('.') > 2 and _VERSION.split('.')[-1] != '0' else "#stable"
 VERSION = _VERSION+VERSION_TYPE
