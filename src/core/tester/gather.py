476b5fb5f2210713f1baf6592fe4f59c74605551
Heisenberg <alimirmohammad.1386@gmail.com>
2024-03-11 16:05:13 -0400

    Removed obsolete modules

--- src/core/tester/gather.py
+++ src/core/tester/gather.py
@@ -78,7 +78,6 @@ from src.data import arg
 
 import urllib3
 def gather_exploit():
-    global xml
     if "?" not in arg.url:
         cr_msg = "No get parameters has been set for injection test.\n example: https://test.php?id=1"
         logger.critical(cr_msg)
