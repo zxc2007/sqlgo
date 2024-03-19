4d43241ecaaa9841a1b4c120cc0b2188a3e2ea57
Heisenberg <alimirmohammad.1386@gmail.com>
2024-03-19 16:30:35 -0400

    related to #62

--- src/core/enums/devstatus.py
+++ src/core/enums/devstatus.py
@@ -22,6 +22,9 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from enum import Enum
 
 class DevStatus(Enum):
+    """
+    This class is used to define the status of the project development.
+    """
     NOT_READY_FOR_USE_IN_BUILD = -1
     NOT_READY_FOR_PRODUCTION_NOT_SAFE_USAGE = 0
     READY_FOR_PRODUCTION_AND_USE = 1
